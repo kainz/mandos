778e63361495666d01b95fdd2d1f907827155331
Teddy Hogeborn <teddy@fukt.bsnet.se>
2010-09-26 20:32:58 +0200

    Update copyright year to "2010" wherever appropriate.
    * DBUS-API: Added copyright and license statement.
    * README: Mention new "plymouth" plugin.
    * debian/control: Depend on python-2.6 or python-multiprocessing.
    * debian/mandos-client.README.Debian: Update info about DEVICE setting
                                          of initramfs.conf.
    * mandos-ctl: Added copyright and license statement.
    * mandos-monitor: - '' -
    * plugins.d/plymouth.c: - '' -

--- plugins.d/plymouth.c
+++ plugins.d/plymouth.c
@@ -1,3 +1,27 @@
+/*  -*- coding: utf-8 -*- */
+/*
+ * Usplash - Read a password from usplash and output it
+ * 
+ * Copyright © 2010 Teddy Hogeborn
+ * Copyright © 2010 Björn Påhlsson
+ * 
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ * 
+ * Contact the authors at <mandos@fukt.bsnet.se>.
+ */
+
 #define _GNU_SOURCE		/* asprintf(), TEMP_FAILURE_RETRY() */
 #include <signal.h>		/* sig_atomic_t, struct sigaction,
 				   sigemptyset(), sigaddset(), SIGINT,
