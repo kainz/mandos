940d6ecc1a5c989fb5d4642698b4925de850965b
Teddy Hogeborn <teddy@recompile.se>
2014-03-29 03:38:15 +0100

    Update copyright year.
    * mandos-keygen: Update copyright year.
    * plugin-runner.c: - '' -
    * plugins.d/askpass-fifo.c: - '' -
    * plugins.d/mandos-client.c: - '' -
    * plugins.d/password-prompt.c: - '' -
    * plugins.d/plymouth.c: - '' -
    * plugins.d/splashy.c: - '' -
    * plugins.d/usplash.c: - '' -

--- plugins.d/splashy.c
+++ plugins.d/splashy.c
@@ -2,8 +2,8 @@
 /*
  * Splashy - Read a password from splashy and output it
  * 
- * Copyright © 2008-2012 Teddy Hogeborn
- * Copyright © 2008-2012 Björn Påhlsson
+ * Copyright © 2008-2014 Teddy Hogeborn
+ * Copyright © 2008-2014 Björn Påhlsson
  * 
  * This program is free software: you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
