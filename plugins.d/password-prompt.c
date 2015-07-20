10b332144b13d61792906dc323cd03b6e6712d0b
Teddy Hogeborn <teddy@recompile.se>
2015-07-20 06:03:32 +0200

    Update copyright year.
    * debian/copyright (Copyright): Update copyright year.
    * intro.xml (COPYRIGHT): - '' -
    * mandos-clients.conf.xml (COPYRIGHT): - '' -
    * mandos-ctl.xml (COPYRIGHT): - '' -
    * mandos-keygen.xml (COPYRIGHT): - '' -
    * mandos-monitor.xml (COPYRIGHT): - '' -
    * mandos.conf.xml (COPYRIGHT): - '' -
    * mandos.xml (COPYRIGHT): - '' -
    * plugin-runner.c: - '' -
    * plugin-runner.xml (COPYRIGHT): - '' -
    * plugins.d/askpass-fifo.c: - '' -
    * plugins.d/askpass-fifo.xml (COPYRIGHT): - '' -
    * plugins.d/mandos-client.xml (COPYRIGHT): - '' -
    * plugins.d/password-prompt.c: - '' -
    * plugins.d/password-prompt.xml (COPYRIGHT): - '' -
    * plugins.d/plymouth.c: - '' -
    * plugins.d/plymouth.xml (COPYRIGHT): - '' -
    * plugins.d/splashy.c: - '' -
    * plugins.d/splashy.xml (COPYRIGHT): - '' -
    * plugins.d/usplash.c: - '' -
    * plugins.d/usplash.xml (COPYRIGHT): - '' -

--- plugins.d/password-prompt.c
+++ plugins.d/password-prompt.c
@@ -2,8 +2,8 @@
 /*
  * Password-prompt - Read a password from the terminal and print it
  * 
- * Copyright © 2008-2014 Teddy Hogeborn
- * Copyright © 2008-2014 Björn Påhlsson
+ * Copyright © 2008-2015 Teddy Hogeborn
+ * Copyright © 2008-2015 Björn Påhlsson
  * 
  * This program is free software: you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
