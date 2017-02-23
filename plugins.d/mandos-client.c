31fe55d5d681ed4ec7327322c265e79070f8d063
Teddy Hogeborn <teddy@recompile.se>
2017-02-23 20:11:11 +0100

    Update copyright year to 2017
    * DBUS-API: Update copyright year to 2017.
    * debian/copyright: - '' -
    * intro.xml: - '' -
    * mandos: - '' -
    * mandos-clients.conf.xml: - '' -
    * mandos-ctl: - '' -
    * mandos-ctl.xml: - '' -
    * mandos-keygen: - '' -
    * mandos-keygen.xml: - '' -
    * mandos-monitor: - '' -
    * mandos-monitor.xml: - '' -
    * mandos.conf.xml: - '' -
    * mandos.xml: - '' -
    * plugin-helpers/mandos-client-iprouteadddel.c: - '' -
    * plugin-runner.c: - '' -
    * plugin-runner.xml: - '' -
    * plugins.d/askpass-fifo.c: - '' -
    * plugins.d/askpass-fifo.xml: - '' -
    * plugins.d/mandos-client.c: - '' -
    * plugins.d/mandos-client.xml: - '' -
    * plugins.d/password-prompt.c: - '' -
    * plugins.d/password-prompt.xml: - '' -
    * plugins.d/plymouth.c: - '' -
    * plugins.d/plymouth.xml: - '' -
    * plugins.d/splashy.c: - '' -
    * plugins.d/splashy.xml: - '' -
    * plugins.d/usplash.c: - '' -
    * plugins.d/usplash.xml: - '' -

--- plugins.d/mandos-client.c
+++ plugins.d/mandos-client.c
@@ -9,8 +9,8 @@
  * "browse_callback", and parts of "main".
  * 
  * Everything else is
- * Copyright © 2008-2016 Teddy Hogeborn
- * Copyright © 2008-2016 Björn Påhlsson
+ * Copyright © 2008-2017 Teddy Hogeborn
+ * Copyright © 2008-2017 Björn Påhlsson
  * 
  * This program is free software: you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
