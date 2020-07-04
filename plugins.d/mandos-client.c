a8403f55e80517292581875c27fd018e2f6be167
teddy@recompile.se <>
2020-07-04 13:58:52 +0200

    Update copyright year
    * DBUS-API: Update copyright year to 2020
    * debian/copyright: - '' -
    * dracut-module/ask-password-mandos.service: - '' -
    * dracut-module/password-agent.c: - '' -
    * mandos: - '' -
    * mandos-ctl: - '' -
    * plugin-runner.c: - '' -
    * plugins.d/mandos-client.c: - '' -
    * plugins.d/plymouth.c: - '' -
    * mandos-to-cryptroot-unlock: Update copyright year to 2019.

--- plugins.d/mandos-client.c
+++ plugins.d/mandos-client.c
@@ -9,8 +9,8 @@
  * "browse_callback", and parts of "main".
  * 
  * Everything else is
- * Copyright © 2008-2019 Teddy Hogeborn
- * Copyright © 2008-2019 Björn Påhlsson
+ * Copyright © 2008-2020 Teddy Hogeborn
+ * Copyright © 2008-2020 Björn Påhlsson
  * 
  * This file is part of Mandos.
  * 
