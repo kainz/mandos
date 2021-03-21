d7e8e18eac927d07a975acf09daca0241abdb9bc
Teddy Hogeborn <teddy@recompile.se>
2021-03-21 19:48:43 +0100

    Break long line (white space change only)
    * plugin-runner.c (main): Break long line.

--- plugin-runner.c
+++ plugin-runner.c
@@ -734,7 +734,8 @@ int main(int argc, char *argv[]){
 	custom_argc += 1;
 	{
 #if defined(__GLIBC_PREREQ) and __GLIBC_PREREQ(2, 26)
-	  char **new_argv = reallocarray(custom_argv, (size_t)custom_argc + 1,
+	  char **new_argv = reallocarray(custom_argv,
+					 (size_t)custom_argc + 1,
 					 sizeof(char *));
 #else
 	  char **new_argv = NULL;
